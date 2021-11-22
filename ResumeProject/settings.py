edfb04deadbf466e4a4e55440d117fbce9e11cd6
fareen341 <59610617+fareen341@users.noreply.github.com>
2021-11-22 11:38:43 +0530

    Update settings.py

--- ResumeProject/settings.py
+++ ResumeProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l+%f!^x^v0w##k*t#s^xa6sdim(sn=zm7=)3h*=rgdsbq06e9n'
+SECRET_KEY = ****
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
